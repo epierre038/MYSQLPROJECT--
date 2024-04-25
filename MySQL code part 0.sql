c116d60fe80f9e5ffcc0453ba029ebe356f57cfc
Emanuel Pierre <121529627+epierre038@users.noreply.github.com>
2024-04-25 15:02:41 -0400

    Add files via upload

---
+++ MySQL code part 0.sql
@@ -0,0 +1,13 @@
+select Publisher, name
+from vgs_sales_project.`vgsales (5)`
+where Publisher like '%Activision%';
+
+select publisher 
+from  vgs_sales_project.`vgsales (5)`
+group by Publisher;
+
+select 
+
+
+
+
