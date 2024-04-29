49b3ad75420c837fa801f1cff7f7aaa67bcd7409
Emanuel Pierre <121529627+epierre038@users.noreply.github.com>
2024-04-29 14:32:37 -0400

    Add files via upload

--- MySQL code part 0.sql
+++ MySQL code part 0.sql
@@ -1,10 +1,10 @@
- -- the basic of knowing SQL 
- -- what if you would like to see all your dataset 
+-- the basic of knowing SQL 
+-- what if you would like to see all your dataset 
   
 select *
 from vgs_sales_project.`vgsales (5)`;
   
-   -- would like to see one or more columes by using the select. 
+-- would like to see one or more columes by using the select. 
 
 select name, platform, year, genre
 from vgs_sales_project.`vgsales (5)`; 
@@ -16,19 +16,25 @@ select Publisher, name
 from vgs_sales_project.`vgsales (5)`
 where Publisher like '%Activision%'; 
 
+-- If you would like to see how sales are doing globaly we can 
 
 select  year, name, Global_Sales
 from vgs_sales_project.`vgsales (5)`
 order by year DESC;
 
+ -- would like to see what games that sold over 3.0 in NA_sale
+ 
 select * 
 from vgs_sales_project.`vgsales (5)`
 where NA_Sales >= '3';
+
+-- The company Atrari what games have they sold?
 
 select *
 from vgs_sales_project.`vgsales (5)`
 where Platform = 'Atari'; 
   
+ -- Stake holder are asking what sales had over 10  in global sales and or was in 2016. 
 
 select *
 from vgs_sales_project.`vgsales (5)`
@@ -36,11 +42,13 @@ where Global_Sales > 10
 or year = '2016'
 ;
 
+ -- what is the avgerage global sales
+
 select avg(global_sales)
 from vgs_sales_project.`vgsales (5)`
-where year = '2016'
 ;
 
+ -- what is the max for global sales 
 
 select max(global_sales) 
 from vgs_sales_project.`vgsales (5)`
